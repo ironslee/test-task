0ff25f2e39717d4ebee3d625d6f55d397aba4362
ironslee <lee.thejuggernaut@gmail.com>
2025-01-19 01:22:34 +0600

    cors anywhere temporary proxy

--- src/utils/axiosMiddleware.ts
+++ src/utils/axiosMiddleware.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios;
 
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+const backendUrl = "https://hcateringback-dev.unitbeandev.com";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 api.interceptors.request.use((config: any) => {
   const tokens = window.localStorage.getItem("tokens");
@@ -13,10 +17,12 @@ api.interceptors.request.use((config: any) => {
     config.headers.authorization = `${access_token}`;
   }
 
-  config.url = new URL(
-    config.url,
-    "https://hcateringback-dev.unitbeandev.com"
-  ).toString();
+  // config.url = new URL(
+  //   config.url,
+  //   "https://hcateringback-dev.unitbeandev.com"
+  // ).toString();
+
+  config.url = new URL(config.url, `${proxyUrl}${backendUrl}`).toString();
 
   return config;
 });
